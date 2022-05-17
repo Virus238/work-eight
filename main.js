61cdc4ff363a1a3ce57ca7757b9985513ae01293
Virus238 <sviatoslav.polon@gmail.com>
2022-05-17 20:32:36 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,23 @@
+/* 1 домашка
+while (true) {
+    let userName = prompt('Ведіть Ігор', '');
+
+    if (userName === 'Ігор') {
+        alert('Ви вели вірно');
+        break;
+    } else if (isNaN(userName)){
+        alert('Ведіть імя ще раз');
+    } else {
+        alert('Ведіть Імя, число - це не правельне значення');
+    } 
+}*/
+
+
+/* 2 домашка
+let numeric = 5;
+
+while (numeric <= 15) {
+    console.log(numeric);
+    i++;
+}*/
+
